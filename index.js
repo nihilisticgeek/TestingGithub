bd0382143056ee34af29b2284f8183d7d0741e34
nihilisticgeek <mode.adam.hussain@gmail.com>
2021-04-20 12:53:32 +0500

    second commit

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function myFunction() {
   var x = document.getElementById("fname");
   x.value = x.value.toUpperCase();
 }
+
+
+let newFunction = () => {
+	let y = document.getElementById('uname');
+	y.value = y.value.toUpperCase();
+}
